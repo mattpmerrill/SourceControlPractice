0a4fb390b97944a78ae3a3a04feaa0f5c276e8b5
mattpmerrill <mattpmerrill@gmail.com>
2018-12-16 11:05:22 -0700

    First commit through Xcode

--- SourceControlPractice/ViewController.swift
+++ SourceControlPractice/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //This is my first test with Git in Xcode
     }
 
 
